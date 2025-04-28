fb44663a2c5f319976e8befa5f5af5135720017a
github-ci <you@example.com>
2025-04-28 09:47:16 +0000

    fix: 修复字段错误

--- src/Entity/Publish.php
+++ src/Entity/Publish.php
@@ -33,7 +33,7 @@ class Publish
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
